a26cb017b4140e67f8fd3ab939d064b25cf178c7
Lorenzo Poggi <lorenzo.poggijanin@gmail.com>
2025-07-01 17:06:57 -0300

    .

--- trabajo_integrador/trabajo_final/armgcc/debug/CMakeFiles/3.30.0/CMakeSystem.cmake
+++ trabajo_integrador/trabajo_final/armgcc/debug/CMakeFiles/3.30.0/CMakeSystem.cmake
@@ -3,12 +3,12 @@ set(CMAKE_HOST_SYSTEM_NAME "Darwin")
 set(CMAKE_HOST_SYSTEM_VERSION "24.5.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
 
-include("/Users/lorenzo.poggi/Documents/GitHub/curso_lse /trabajo_integrador/sdks/trabajo_final_sdk/tools/cmake_toolchain_files/armgcc.cmake")
+
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_SYSTEM_VERSION "")
-set(CMAKE_SYSTEM_PROCESSOR "arm")
+set(CMAKE_SYSTEM_PROCESSOR "")
 
 set(CMAKE_CROSSCOMPILING "TRUE")
 
